c21e7efd4c9e59916a52fa70f5d84b7efa174467
Feaxmutch <feaxmutch@gmail.com>
2024-10-19 13:13:29 +0300

    Initial commit

---
+++ QueueInShop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace QueueInShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
